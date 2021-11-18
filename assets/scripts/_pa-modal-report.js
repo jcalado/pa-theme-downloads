9a62c4450a67dded8bcbd4bda540cc5042e1c527
Tiago Papile <tiagopapile@gmail.com>
2021-11-18 11:31:29 -0300

    ad-63 Finaliza form

--- assets/scripts/_pa-modal-report.js
+++ assets/scripts/_pa-modal-report.js
@@ -4,15 +4,61 @@ export function pa_modal_report() {
 	const form = document.getElementById('form-report');
   const report_permalink = document.getElementById('report-permalink');
   let request = null;
+  let data = null;
 
   if(!form || !modal || !modal_callback)
     return;
+
+  const pa_manage_form = (sending) => {
+    const button = form.querySelector('.form-report__button');
+  
+    if(!button)
+      return;
+  
+    button.toggleAttribute('disabled', sending);
+    button.querySelector('.form-report__spinner')?.classList.toggle('visually-hidden', !sending);
+    button.querySelector('.form-report__sending')?.classList.toggle('visually-hidden', !sending);
+    button.querySelector('.form-report__text')?.classList.toggle('visually-hidden', sending);
+  };
+
+  const pa_report_token = () => {
+    return window.grecaptcha.execute(window.recaptcha.site_key ? window.recaptcha.site_key : '', {action: 'submit'});
+  };
+
+  const pa_send_report = (data) => {
+    const request = new XMLHttpRequest();
+    data.action = 'send_report';
+  
+    request.responseType = 'json';
+    request.open('POST', window.pa.url, true);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    request.send(Object.keys(data).map(key => key + '=' + data[key]).join('&'));
+  
+    return request;
+  };
+
+  const send = () => {
+    request = pa_send_report(data);
+    request.onloadend = (request) => { 
+      if(request.target.readyState !== 4 || request.target.status !== 200)
+        return;
+
+      this.request = null;
+      pa_manage_form(false);
+
+      modal_callback.classList.toggle('error', request.target.status !== 200 || !request.target.response.success);
+      modal_callback.classList.toggle('success', request.target.status === 200 && request.target.response.success);
+  
+      window.bootstrap.Modal.getOrCreateInstance(modal).hide();
+      window.bootstrap.Modal.getOrCreateInstance(modal_callback).show();
+    };
+  };
   
   modal.addEventListener('show.bs.modal', (event) => {
-    request?.abort()
+    request?.abort();
     form.reset();
     form.classList.remove('was-validated');  
-    pa_manage_form(form, false);
+    pa_manage_form(false);
       
     if(report_permalink)
       report_permalink.value = event.relatedTarget.getAttribute('data-bs-permalink');
@@ -28,53 +74,17 @@ export function pa_modal_report() {
       return;
 
     pa_manage_form(form, true);
-      
-    pa_report_token().then((token) => {
-      let data = Object.fromEntries(new FormData(form));
-      data.token = token;
-
-      request = pa_send_report(data);
-      request.onloadend = (request) => { 
-        if(request.target.readyState !== 4 || request.target.status !== 200)
-          return;
-
-        this.request = null;
-        pa_manage_form(form, false);
-
-        modal_callback.classList.toggle('error', request.target.status !== 200 || !request.target.response.success);
-        modal_callback.classList.toggle('success', request.target.status === 200 && request.target.response.success);
     
-        window.bootstrap.Modal.getOrCreateInstance(modal).hide();
-        window.bootstrap.Modal.getOrCreateInstance(modal_callback).show();
-      };
-    });
+    data = Object.fromEntries(new FormData(form));
+
+    if(window.grecaptcha) {
+      pa_report_token().then((token) => {
+        data.token = token;
+  
+        send();
+      });
+    }
+    else
+      send();
   }, false);
-}
-
-function pa_report_token() {
-  return window.grecaptcha.execute('6LfdTS0dAAAAAMZRMBNJqSAvv7hnC7KGWmRffpY3', {action: 'submit'});
-}
-
-function pa_send_report(data) {
-  const request = new XMLHttpRequest();
-  data.action = 'send_report';
-
-  request.responseType = 'json';
-  request.open('POST', window.pa.url, true);
-  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-  request.send(Object.keys(data).map(key => key + '=' + data[key]).join('&'));
-
-  return request;
-}
-
-function pa_manage_form(form, sending) {
-  const button = form.querySelector('.form-report__button');
-
-  if(!button)
-    return;
-
-  button.toggleAttribute('disabled', sending);
-  button.querySelector('.form-report__spinner')?.classList.toggle('visually-hidden', !sending);
-  button.querySelector('.form-report__sending')?.classList.toggle('visually-hidden', !sending);
-  button.querySelector('.form-report__text')?.classList.toggle('visually-hidden', sending);
 }
--- classes/controllers/PA_Ajax.class.php
+++ classes/controllers/PA_Ajax.class.php
@@ -7,25 +7,29 @@ class PAAjax {
   }
 
   function sendReport() {
-    $secret = '6LfdTS0dAAAAAJAYh8XhzssK5-ENZGMJ-oLPsNJz';
-    $token = $_POST['token'];
-    $url = 'https://www.google.com/recaptcha/api/siteverify';
-    $params = "secret={$secret}&response={$token}";
-    
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    
-    $response = json_decode(curl_exec($ch), true);
+    $secret = get_field('report_recaptcha_secret_key', 'option');
+    $token = sanitize_text_field(array_key_exists('token', $_POST) ? $_POST['token'] : '');
 
-    if(!is_array($response) || 
-      !array_key_exists('success', $response) || 
-      empty($response['success']) || 
-      $response['score'] < 0.5)
-      wp_send_json_error();
+    if(!empty($secret) && !empty($token)):
+      // var_dump(123);
+      $url = 'https://www.google.com/recaptcha/api/siteverify';
+      $params = "secret={$secret}&response={$token}";
+      
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_POST, 1);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+      curl_setopt($ch, CURLOPT_HEADER, 0);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      
+      $response = json_decode(curl_exec($ch), true);
+
+      if(!is_array($response) || 
+        !array_key_exists('success', $response) || 
+        empty($response['success']) || 
+        $response['score'] < 0.5)
+        wp_send_json_error();
+    endif;
 
     $mail = wp_mail(
       get_field('report_email', 'option'), 
