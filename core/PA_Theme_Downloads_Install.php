7336a38cd3c88ee4455eea94bb2cdaeabac0e920
Eli Mendonça <contato@elimendonca.com>
2021-11-24 10:43:46 -0300

    - Ajuste nas regras de URL dos posts - Registra a taxonomia "tipo de materiais" - Ajuste pequeno na borda da lista de downloads

--- core/PA_Theme_Downloads_Install.php
+++ core/PA_Theme_Downloads_Install.php
@@ -56,6 +56,32 @@ class PAThemeDownloadsInstall {
 		register_taxonomy_for_object_type('xtt-pa-projetos', 'kit');
     register_taxonomy_for_object_type('xtt-pa-departamentos', 'kit');
     register_taxonomy_for_object_type('xtt-pa-owner', 'kit');
+
+    $labels = array(
+			'name'                => __( 'Tipos de Materiais', 'iasd'),
+			'singular_name'       => __( 'Tipo de Material', 'iasd'),
+			'search_items'        => __( 'Buscar Tipo de Material', 'iasd'),
+			'all_items'           => __( 'Todos os Tipos de Materiais', 'iasd'),
+			'parent_item'         => __( 'Tipo Superior', 'iasd'),
+			'parent_item_colon'   => __( 'Tipo Superior:', 'iasd'),
+			'edit_item'           => __( 'Editar Tipo de Material', 'iasd' ),
+			'update_item'         => __( 'Atualizar Tipo de Material', 'iasd'),
+			'add_new_item'        => __( 'Adicionar Novo Tipo de Material', 'iasd'),
+			'new_item_name'       => __( 'Nome do Tipo de Material', 'iasd'),
+			'menu_name'           => __( 'Tipos de Materiais', 'iasd')
+		);
+
+		$args = array(
+			'hierarchical'        => true,
+			'labels'              => $labels,
+			'show_ui'             => true,
+			'show_admin_column'   => false,
+			'query_var'           => true,
+			'rewrite'             => array( 'slug' => __('tipo-material', 'iasd') ),
+			'public'              => true,
+		);
+
+    register_taxonomy( 'xtt-pa-materiais', array('post'), $args );
 	}
 
 	function setWidgets() {
@@ -63,7 +89,6 @@ class PAThemeDownloadsInstall {
 		unregister_sidebar('index');
 		unregister_sidebar('single');
 	}
-
 }
 
 $PAThemeDownloadsInstall = new PAThemeDownloadsInstall();
--- template-parts/single/list-downloads.blade.php
+++ template-parts/single/list-downloads.blade.php
@@ -3,20 +3,20 @@
     <caption class="d-none"><?= __('Download', 'iasd')  ?></caption>
     <thead>
       <tr>
-        <th class="px-0 border-0 d-none d-lg-table-cell" scope="col">{{ __('Name', 'iasd') }}</th>
+        <th class="px-3 border-0 d-none d-lg-table-cell" scope="col">{{ __('Name', 'iasd') }}</th>
 
         <th class="px-3 border-0 d-none d-lg-table-cell" scope="col">{{ __('Size', 'iasd') }}</th>
 
-        <th class="pa-downloads-table__fit px-0 border-0 text-end d-none d-lg-table-cell" scope="col">{{ __('Download', 'iasd') }}</th>
+        <th class="pa-downloads-table__fit px-3 border-0 text-end d-none d-lg-table-cell" scope="col">{{ __('Download', 'iasd') }}</th>
 
-        <th class="px-0 border-0 d-block d-lg-none" scope="col">{{ __('Files for download', 'iasd') }}</th>
+        {{-- <th class="px-0 border-0 d-block d-lg-none" scope="col">{{ __('Files for download', 'iasd') }}</th> --}}
       </tr>
     </thead> 
 
     <tbody>
       @foreach($downloads as $download)
         <tr>
-          <td class="py-3 px-0 pe-lg-3">
+          <td class="py-3 px-3 pe-lg-3">
             <span class="pa-truncate-1" title="{!! $loop->iteration . '. ' . wp_strip_all_tags($download['name']) !!}">
               <span class="pa-tag rounded-1 text-uppercase d-inline-block px-1 me-2">{{ $download['format'] }}</span>
               {!! $loop->iteration . '. ' . wp_strip_all_tags($download['name']) !!}
