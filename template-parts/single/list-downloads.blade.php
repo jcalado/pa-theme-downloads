8d639adcbed0c0d07e2f4a713e1bf6d320c866d6
mizael isaltino <mizael.isaltino@gmail.com>
2022-03-09 13:27:52 -0300

    - Implementando função para evitar que caracteres não UTF-8 sejam adicionado.

--- template-parts/single/list-downloads.blade.php
+++ template-parts/single/list-downloads.blade.php
@@ -26,10 +26,10 @@
           <td class="p-3">{{ $download['size'] . 'MB' }}</td>
 
           <td class="pa-downloads-table__fit py-3 px-0 px-lg-3 fw-bold">
-            <a class="text-decoration-none d-flex align-items-center" href="{{ $download['link'] }}" download target="_blank">
+            <a class="text-decoration-none d-flex align-items-center" href="{{ utf8_decode($download['link']) }}" download target="_blank">
               <i class="fas fa-download me-2"></i>
               
-              <span class="d-none d-lg-inline-block"><?= __('Download', 'iasd')  ?></span> 
+              <span class="d-none d-lg-inline-block"><?= __('Download', 'iasd')  ?></span>
             </a>
           </td>
         </tr>
